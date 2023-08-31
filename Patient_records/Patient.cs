4dc9f07468e60153b12bc63a7891f68933e7889d
SoujanyaND <143153675+SoujanyaND@users.noreply.github.com>
2023-08-31 21:59:33 +0530

    Add files via upload

---
+++ Patient_records/Patient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Patient_serialization
+{
+    [Serializable]
+    public class Patient
+    {
+        public int patient_id { get; set; }
+        public string patient_name { get; set; }
+        public string address { get; set; }
+        public long phone { get; set; }
+        public DateTime JoiningDate { get; set; } = DateTime.Now.Date;
+        public string doctorName { get; set; }
+        public float Bill_amount { get; set; }
+    }
+    
+}
+    
+    
+
---
+++ Patient_records/PatientSerializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Patient_serialization
+{
+    
+        public class PatientSerializer
+        {
+
+            public static void Serialize(List<Patient> patient, string filePath)
+            {
+            FileStream fs = new FileStream("Data.xml", FileMode.OpenOrCreate, FileAccess.Write);
+                XmlSerializer fm = new XmlSerializer(typeof(List<Patient>));
+            fm.Serialize(fs, patient);
+            fs.Close();
+            
+                
+            }
+            public static List<Patient> Deserialize(string filePath)
+            {
+            FileStream fs = new FileStream("Data.xml", FileMode.Open, FileAccess.Read);
+            XmlSerializer fm = new XmlSerializer(typeof(List<Patient>));
+            foreach (var item in ProgramMain.patients)
+                Console.WriteLine(item);
+            fs.Close();
+            return ProgramMain.patients;
+            
+
+        }
+    }
+    }
+
