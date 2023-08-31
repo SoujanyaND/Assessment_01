bd449758b620e5f7cf2caaeeb112d3a9c4dab8c9
SoujanyaND <143153675+SoujanyaND@users.noreply.github.com>
2023-08-31 15:28:36 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Patient_serialization
+{
+    [Serializable]
+    public class Program
+    {
+        public int patient_id { get; set; }
+        public string patient_name { get; set; }
+        public string address { get; set; }
+        public long phone { get; set; }
+        public DateTime JoiningDate { get; set; } = DateTime.Now.Date;
+        public string doctorName { get; set; }
+        public float Bill_amount { get; set; }
+        public override string ToString()
+        {
+            return $"The Patient information is as follows : \n Patient id :{patient_id}\n patient name {patient_name}\n patient address {address} \n patient phone number : {phone} \n patient admission date {JoiningDate.ToLongDateString()}\n doctor name {doctorName} \ntotal bill amount {Bill_amount} ";
+        }
+        
+            private static void xmlSerialization()
+            {
+                
+                Program data = CreateData();
+                
+                FileStream fs = new FileStream("PatientRecord.xml", FileMode.OpenOrCreate, FileAccess.Write);
+                XmlSerializer fm = new XmlSerializer(typeof(Program));
+                fm.Serialize(fs, data);
+                fs.Close();
+                Console.WriteLine("Xml Serialization successfully completed");
+            }
+            private static void XmlDeserialization()
+            {
+                FileStream fs = new FileStream("PatientRecord.xml", FileMode.Open, FileAccess.Read);
+                XmlSerializer fm = new XmlSerializer(typeof(Program));
+                Program extracted = fm.Deserialize(fs) as Program;
+                Console.WriteLine(extracted.ToString());
+            }
+
+        //create
+            public static Program CreateData()
+            {
+                int pid = Utilities.GetInteger("Enter the Patient id");
+                string pname = Utilities.GetString("Enter the Patient name");
+                string paddress = Utilities.GetString("Enter the Patient address");
+                long pPhone = Utilities.GetLong("Enter the Patient phone number");
+                DateTime jDate = Utilities.GetDateTime("Enter the Date ");
+                float bill = Utilities.GetFloat("Enter the Bill Amount");
+
+                return new Program
+                {
+                    patient_id = pid,
+                    patient_name = pname,
+                    address = paddress,
+                    phone = pPhone,
+                    JoiningDate = jDate,
+                    Bill_amount = bill
+                };
+
+            }
+        //delete
+        public static void deletedata()
+        {
+            File.Delete("PatientRecord.xml");
+            Console.WriteLine("The file was deleted successfully" 
+                );
+        }
+        
+        static void Main(string[] args)
+            {
+                bool repeat = true;
+                while (repeat)
+                {
+                    int choice = Utilities.GetInteger("1.Insert Patient Record 2.Display Patient 3.remove Patient record  Record 4.Update Patient");
+                    switch (choice)
+                    {
+                        case 1: { 
+                                xmlSerialization(); } break;
+                        case 2: XmlDeserialization();break;
+                        case 3: deletedata();break;
+                        default: Console.WriteLine("Wrong input"); break;
+
+                    }
+                }
+
+
+                
+            }
+        }
+    }
+
+
---
+++ Utilities.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patient_serialization
+{
+    class Utilities
+    {
+        public static int GetInteger( string question)
+        {
+            Console.WriteLine(question);
+            return int.Parse(Console.ReadLine());
+        }
+        public static string GetString(string question)
+        {
+            Console.WriteLine(question);
+            return Console.ReadLine();
+        }
+        public static double GetDouble(string question)
+        {
+            Console.WriteLine(question);
+            return double .Parse(Console.ReadLine());
+        }
+        public static long GetLong(string question)
+        {
+            Console.WriteLine(question);
+            return long.Parse(Console.ReadLine());
+        }
+        public static float GetFloat(string question)
+        {
+            Console.WriteLine(question);
+            return float.Parse(Console.ReadLine());
+        }
+        public static DateTime GetDateTime(string question)
+        {
+            Console.WriteLine(question);
+            return DateTime.Parse(Console.ReadLine());
+        }
+    }
+}
